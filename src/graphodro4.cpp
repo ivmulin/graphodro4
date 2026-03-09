54e30db8663cbdc517e887bc9e7fbeafcfa4943d
ivanozavr0 <ivankarpov951@gmail.com>
2026-03-09 17:57:31 +0300

    Added parser / rmEdges testcase

--- src/graphodro4.cpp
+++ src/graphodro4.cpp
@@ -18,12 +18,25 @@ int main() {
         g.addEdge(2, 4);
         g.addEdge(1, 2);
     */
-
     Graph g;
 
     EdgeListParser elp;
     elp.parse("samples/edgelist1.txt", g);
     std::cout << g << "\n";
+
+    g.addEdge(1, 2);
+    g.addEdge(1, 2);
+
+    std::cout << "Added two more edges (1, 2):\n" << g << "\n";
+    std::cout << "deg(1) = " << g.deg(1) << "\n";
+
+    g.rmEdges(2, 1);
+
+    std::cout << "Removed edge (1, 2):\n" << g << "\n";
+
+    g.rmEdges(1, 2, 10);
+
+    std::cout << "Removed edges (1, 2):\n" << g << "\n";
 
     std::cout << "Fin!\n";
     return 0;
