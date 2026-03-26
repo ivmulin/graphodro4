34405bec8d18b1debff814ca3648940e97f4c3e6
ivanozavr0 <ivankarpov951@gmail.com>
2026-03-26 15:49:19 +0300

    Added minor enhancements

---
+++ include/graphs.hpp
@@ -0,0 +1,7 @@
+#ifndef GRAPH_DEFINITIONS_HPP
+#define GRAPH_DEFINITIONS_HPP
+
+#include "adjacency_list.hpp"
+#include "adjacency_matrix.hpp"
+
+#endif  // GRAPH_DEFINITIONS_HPP
--- include/igraph.hpp
+++ include/igraph.hpp
@@ -2,7 +2,6 @@
 #define IGRAPH_HPP
 
 #include <cstddef>
-#include <cstdint>
 #include <iostream>
 #include <vector>
 
@@ -11,8 +10,14 @@
  * Требуется для реализации интерфейсов.
  */
 class IGraph {
+   private:
+    size_t p_n;
+    size_t p_edges;
+
    public:
     virtual ~IGraph() = default;
+
+    virtual void allocate(size_t n) = 0;
 
     /** @brief Возвращает количество вершин в графе */
     virtual size_t getVerticesCount() const = 0;
@@ -26,8 +31,18 @@ class IGraph {
     /** @brief Возвращает количество ребер в графе */
     virtual size_t getE() const = 0;
 
+    /** @brief Добавляет невзвешенное ребро */
+    virtual size_t addEdge(size_t from, size_t to) = 0;
+
+    size_t addMultipleEdges(size_t from, size_t to, size_t count) {
+        for (size_t i = 0; i < count; i++) {
+            addEdge(from, to);
+        }
+        return p_edges;
+    }
+
     /** @brief Добавляет k вершин к графу */
-    virtual size_t addVertices(uint8_t k = 1) = 0;
+    virtual size_t addVertices(size_t k = 1) = 0;
 
     /** @brief Метод, определяющий, как конкретная реализация наследников будет
      * отображаеться в консоли */
@@ -48,9 +63,6 @@ class IUnweightedGraph : virtual public IGraph {
      * Интерфейс для реализации невзвешенного графа
      */
    public:
-    /** @brief Добавляет невзвешенное ребро */
-    virtual size_t addEdge(size_t from, size_t to) = 0;
-
     /** @brief Удаляет невзвешенное ребро */
     virtual size_t rmEdges(size_t from, size_t to, size_t occurrences = 1) = 0;
 
@@ -59,21 +71,6 @@ class IUnweightedGraph : virtual public IGraph {
 
     /** @brief Получает степень вершины */
     virtual size_t deg(size_t vertex) const = 0;
-};
-
-/**
- * @brief Интерфейс для взвешенных графов
- * @tparam T Численный тип для весов (int, double и т.д).
- */
-template <typename T>
-class IWeightedGraph : virtual public IGraph {
-   public:
-    /** @brief Добавляет взвешенное ребро */
-    virtual void addEdge(size_t from, size_t to, T weight) = 0;
-
-    /** @brief Возвращает пару (сосед, вес). */
-    virtual std::vector<std::pair<size_t, T>> getWeightedNeighbors(
-        size_t vertex) const = 0;
 };
 
 #endif  // IGRAPH_HPP
