0f69d3427d5321ad8866910c60428ecdd7b0b699
andruha2007 <andruha141207@gmail.com>
2026-03-31 00:14:43 +0300

    Serialization was complited!

--- src/graphodro4.cpp
+++ src/graphodro4.cpp
@@ -3,9 +3,12 @@
 #include "_debugging.hpp"
 #include "adjacency_list.hpp"
 #include "parsers.hpp"
+#include "graphviz_serializer.hpp"
 
 using Graph = AdjacencyList;
 using Parser = EdgeListParser;
+using Serializer = GraphVizSerializer;
+
 
 int main() {
     std::cout << "Hello!\n";
@@ -13,7 +16,7 @@ int main() {
     Graph g;
     Parser parser;
 
-    parser.parse("samples/edgelist1.txt", g);
+    parser.parse("../samples/edgelist1.txt", g);
     std::cout << g.getV() << " vertices, " << g.getEdgesCount() << " edges!\n";
 
     for (int i = 0; i < 5; i++) {
@@ -23,5 +26,22 @@ int main() {
     }
 
     std::cout << "Fin!\n";
+
+    try {
+        Graph g;
+        Parser{}.parse("../samples/edgelist1.txt", g);
+
+        std::cout << "Loaded: " << g.getV() << " vertices, " 
+                  << g.getEdgesCount() << " edges\n";
+
+        Serializer(g).saveToFile("output.dot", "Graph");
+        std::cout << "Saved to output.dot\n";
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
     return 0;
 }
